818cca3f137b8ab30810651ee710749b8da1b98f
FloRobart <FlorisRobart.pro@gmail.com>
2024-07-27 11:57:32 +0200

    correction du bug de la photo de profil

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -38,10 +38,10 @@ Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
 
             <div class="smallRowCenterContainer">
                 <!-- Icône du profil -->
-                @if (auth()->check() && auth()->user()->image_extention !== null)
-                    <img class="normalIcons rounded-full" src="{{ asset('storage/profil_image/profil_image_' . auth()->user()->id . '.' . auth()->user()->image_extention) }}" alt="Image de profil">
-                @else
-                    @if (auth()->check())
+                @if (auth()->check())
+                    @if (auth()->user()->imgProfil !== null)
+                        <img class="w-full h-full object-cover" src="data:image/png;base64,{{ auth()->user()->imgProfil }}" alt="Image de profil">
+                    @else
                         <svg class="normalIcons colorFontBleuLogo" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
