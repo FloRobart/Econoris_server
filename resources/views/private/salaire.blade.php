89803e97b683239d2541c2bca9e2beb21e4358b9
FloRobart <FlorisRobart.pro@gmail.com>
2024-11-03 16:34:41 +0100

    corection d'affichage

--- resources/views/private/salaire.blade.php
+++ resources/views/private/salaire.blade.php
@@ -73,7 +73,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                 </svg>
 
-                <span class="tooltiptext">L'indice de gestion d'argent vous indique si vous géré correctement votre argent. Plus l'indice est élevé plus gérer correctement votre argent. Quand l'indice est à 1 ça veux dire que vous dépensé tous se que vous gagné et si l'indice est en dessous de 1 ça veux dire que vous dépensé plus que se que vous gagné.</span>
+                <span class="tooltiptext hidden">L'indice de gestion d'argent vous indique si vous géré correctement votre argent. Plus l'indice est élevé plus gérer correctement votre argent. Quand l'indice est à 1 ça veux dire que vous dépensé tous se que vous gagné et si l'indice est en dessous de 1 ça veux dire que vous dépensé plus que se que vous gagné.</span>
             </div>
         </div>
     </div>
