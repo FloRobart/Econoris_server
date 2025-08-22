a3773b7c5075e0998abf0999b2de89f09f8ab0aa
Floris Robart <floris.robart@qowisio.com>
2025-08-22 16:44:44 +0200

    update config

--- src/main/config/config.ts
+++ src/main/config/config.ts
@@ -11,28 +11,31 @@ interface Config {
     readonly app_name: string;
     readonly app_local: string;
     readonly admin_email?: string;
+
     readonly db_uri: string;
 
-    readonly handshake_static_token?: string;
     private_token?: string; // FlorAccess private token
     private_timestamp?: number; // FlorAccess private timestamp
+    readonly handshake_static_token?: string;
     readonly auth_app_handshake_url: string;
-    auth_app_url?: string;
-    
-    readonly hash_algorithm?: string; // Hash algorithm for security, default is sha256
+    readonly auth_app_url: string;
+
+    readonly hash_algorithm: string; // Hash algorithm for security, default is sha256
     readonly hash_rounds: number;
 }
 
-const DEFAULT_PORT: number = 8080;
+
 const config: Config = {
-    base_url: process.env.BASE_URL || ("http://localhost:" + (normalizePort(process.env.APP_PORT || String(DEFAULT_PORT)) || DEFAULT_PORT)),
-    app_port: normalizePort(process.env.APP_PORT || String(DEFAULT_PORT)) || DEFAULT_PORT,
+    base_url: process.env.BASE_URL || "http://localhost",
+    app_port: 80,
     host_name: process.env.HOST_NAME || 'localhost',
     app_env: process.env.APP_ENV || 'default',
     app_name: process.env.APP_NAME || 'Éconoris',
     app_local: process.env.APP_LOCAL || 'en_US',
     admin_email: process.env.ADMIN_EMAIL || '',
+
     db_uri: process.env.DB_URI || '',
+
     handshake_static_token: process.env.HANDSHAKE_STATIC_TOKEN || '',
     auth_app_handshake_url: process.env.AUTH_APP_HANDSHAKE_URL || '',
     auth_app_url: process.env.AUTH_APP_URL || '',
