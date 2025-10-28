8c5aa0cb96cebfd59e07a9077fa354128efe97a9
FloRobart <FlorisRobart.pro@gmail.com>
2025-10-28 11:07:36 +0100

    add auth type and schema

--- src/core/middlewares/validators/auth_validator.middleware.ts
+++ src/core/middlewares/validators/auth_validator.middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { ZodType } from "zod";
 import { AppError } from "../../models/AppError.model";
 import { AuthorizationHeaderSchema } from "../../../modules/auth/auth.schema";
+import { UserSafe } from "../../../modules/auth/auth.types";
 
 
 
@@ -14,8 +15,20 @@ export const authorizationValidator = async (req: Request, _res: Response, next:
     try {
         const authHeader = AuthorizationHeaderSchema.parse(req.headers.authorization);
         const token = authHeader.split(" ")[1];
+
         // TODO: const user = await AuthService.verifyToken(token);
-        const user = { id: token.split(".")[0] }; // Mock user extraction from token
+        const user: UserSafe = {
+            id: Number(token.split(".")[0]),
+            email: "",
+            pseudo: "",
+            auth_methods_id: 0,
+            is_connected: false,
+            is_verified_email: false,
+            last_login: new Date(),
+            created_at: new Date(),
+            updated_at: new Date()
+        }; // Mock user extraction from token
+
         req.body = { ...req.body, user };
 
         next();
